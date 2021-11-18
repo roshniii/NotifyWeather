0c84672b62e023c2effb5779d5836ff6595905eb
roshni <roshnimalika22@gmail.com>
2021-11-18 15:51:45 +0530

    second commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Weather App
+
+A weather app created with React Js and WeatherStack API.
+
+Link to the video : https://www.youtube.com/watch?v=M-0V5ROegYg&t=2359s
---
+++ build/precache-manifest.cf0a2bd2cb24f67edb85b721029e1ddb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db4501588b3cc763d3741417e83bbb82",
+    "url": "/Bishwahangdewan/Weather-App/index.html"
+  },
+  {
+    "revision": "f071084001aafed7cb3c",
+    "url": "/Bishwahangdewan/Weather-App/static/css/main.b4c6494c.chunk.css"
+  },
+  {
+    "revision": "0b855d1c8cf44aa3e33a",
+    "url": "/Bishwahangdewan/Weather-App/static/js/2.a2afc989.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Bishwahangdewan/Weather-App/static/js/2.a2afc989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f071084001aafed7cb3c",
+    "url": "/Bishwahangdewan/Weather-App/static/js/main.a864c242.chunk.js"
+  },
+  {
+    "revision": "577ab63d266c291e92ef",
+    "url": "/Bishwahangdewan/Weather-App/static/js/runtime-main.3eb1cdb3.js"
+  },
+  {
+    "revision": "b33ceb7ff2f1dd9a0408ec6ba97b8caa",
+    "url": "/Bishwahangdewan/Weather-App/static/media/weather.b33ceb7f.jpeg"
+  }
+]);
